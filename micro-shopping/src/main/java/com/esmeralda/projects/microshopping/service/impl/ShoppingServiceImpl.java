a081f65a325bc6366aece87c7d98694994d11416
Robin Arellano <robinarehen@gmail.com>
2022-08-26 12:53:45 +0200

    se adiciona el restTemplate de compras y el crud correspondiente

--- micro-shopping/src/main/java/com/esmeralda/projects/microshopping/service/impl/ShoppingServiceImpl.java
+++ micro-shopping/src/main/java/com/esmeralda/projects/microshopping/service/impl/ShoppingServiceImpl.java
@@ -1,11 +1,16 @@
 package com.esmeralda.projects.microshopping.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.esmeralda.projects.microshopping.entity.ShoppingEntity;
+import com.esmeralda.projects.microshopping.model.ProductVo;
 import com.esmeralda.projects.microshopping.model.ShoppingVo;
 import com.esmeralda.projects.microshopping.repository.ShoppingRepository;
 import com.esmeralda.projects.microshopping.service.ShoppingService;
@@ -16,18 +21,60 @@ public class ShoppingServiceImpl implements ShoppingService {
 
 	private ShoppingRepository shoppingRepository;
 	private MapperUtil mapperUtil;
+	private RestTemplate restTemplate;
 
 	public ShoppingServiceImpl(ShoppingRepository shoppingRepository, MapperUtil mapperUtil) {
 		super();
 		this.shoppingRepository = shoppingRepository;
 		this.mapperUtil = mapperUtil;
+		this.restTemplate = new RestTemplate();
 	}
+
+	/**
+	 * 1.si el producto existe se actualiza la cantidad y precios
+	 * 
+	 */
 
 	@Override
 	public ShoppingVo createShopping(ShoppingVo shoppingVo) {
+
+		ProductVo productVo = getproductId(shoppingVo);
+
 		ShoppingEntity shoppingEntity = this.mapperUtil.mapperObject(shoppingVo, ShoppingEntity.class);
 		ShoppingEntity shoppingSaved = this.shoppingRepository.save(shoppingEntity);
+
+		updateProductByid(productVo, shoppingVo);
+
 		return this.mapperUtil.mapperObject(shoppingSaved, ShoppingVo.class);
+	}
+
+	private Map<String, Integer> getUrlVariable(Integer id) {
+		Map<String, Integer> urlVariable = new HashMap<>();
+		urlVariable.put("id", id);
+		return urlVariable;
+	}
+
+	public ProductVo getproductId(ShoppingVo shoppingVo) {
+		ResponseEntity<ProductVo> response = this.restTemplate.getForEntity("http://localhost:8080/product/{id}",
+				ProductVo.class, this.getUrlVariable(shoppingVo.getIdproduct()));
+
+		ProductVo productVo = response.getBody();
+		if (productVo == null) {
+			throw new IllegalArgumentException("debe crear primero el producto");
+		}
+
+		if (shoppingVo.getQuantity() <= 0) {
+			throw new IllegalArgumentException("La cantidad debe ser mayor a cero");
+		}
+		return productVo;
+	}
+
+	public void updateProductByid(ProductVo productVo, ShoppingVo shoppingVo) {
+		Integer quantity = productVo.getQuantity() + shoppingVo.getQuantity();
+		productVo.setQuantity(quantity);
+
+		this.restTemplate.put("http://localhost:8080/product/{id}", productVo,
+				this.getUrlVariable(shoppingVo.getIdproduct()));
 	}
 
 	@Override
